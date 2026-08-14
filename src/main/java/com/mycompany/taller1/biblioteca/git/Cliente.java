6cbffd20bb94601db7c441324d6b90b1e125110b
Angel Miguel Pirela Bueno <apirela@unicesar.edu.co>
2026-08-14 17:34:31 -0500

    feat: crear clase Cliente heredando de Persona

---
+++ src/main/java/com/mycompany/taller1/biblioteca/git/Cliente.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.taller1.biblioteca.git;
+
+/**
+ *
+ * @author apire
+ */
+public class Cliente extends Person{
+     private String email;
+    public Cliente(String id, String name, String telephone, String email) {
+        super(id, name, telephone);
+        this.email=email;
+    }
+    public String getEmail(){return email;}
+    public void setEmail(String email){this.email=email;}
+    
+}
