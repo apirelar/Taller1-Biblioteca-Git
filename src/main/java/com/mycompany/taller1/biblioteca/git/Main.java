e91b0e64f480fea4daf913daeb7bf4d99e965ef4
Angel Miguel Pirela Bueno <apirela@unicesar.edu.co>
2026-08-14 18:15:41 -0500

    feat: implementar Read de Cliente (buscar)

--- src/main/java/com/mycompany/taller1/biblioteca/git/Main.java
+++ src/main/java/com/mycompany/taller1/biblioteca/git/Main.java
@@ -17,7 +17,24 @@ public class Main {
          System.out.println(client);
      }
  }
- 
+ static void ReadSearch(){
+     boolean found=false;
+     System.out.println("***** Search for a client *****");
+     System.out.println("Enter customer ID:");
+     String id=sc.nextLine();
+     for(Client client: clients){
+         if (client.getId().equals(id)) {
+             System.out.println("Client found");
+             System.out.println("Name: "+client.getName());
+             System.out.println("Telephone: "+client.getTelephone());
+             System.out.println("Email: "+client.getEmail());
+             found=true;
+         }
+         if(found==false){
+             System.out.println("Client not found");
+         }
+     } 
+ }
  public static void main(String[] args) {
  // Aquí irá el menú (Fase 8)
  }
