72e1fe5c8eabf520cf87368f19b68d98f83d2d88
Angel Miguel Pirela Bueno <apirela@unicesar.edu.co>
2026-08-14 19:26:20 -0500

    feat: crear clase Libro heredando de Material

---
+++ src/main/java/com/mycompany/biblioteca/Book.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.biblioteca;
+
+/**
+ *
+ * @author apire
+ */
+public class Book extends Material{
+    
+    public Book(String code, String tite, String anioPublic) {
+        super(code, tite, anioPublic);
+    }
+    
+}
