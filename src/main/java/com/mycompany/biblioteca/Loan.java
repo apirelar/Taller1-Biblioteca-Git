9d08a3139f13bb5d8aedff79248dd0e8fd3f5afb
Angel Miguel Pirela Bueno <apirela@unicesar.edu.co>
2026-08-17 16:53:40 -0500

    feat: Crear clase Prestamo que relaciona Cliente y Libro

---
+++ src/main/java/com/mycompany/biblioteca/Loan.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.biblioteca;
+import java.time.LocalDate;
+/**
+ *
+ * @author apire
+ */
+public class Loan {
+    private String idLoan;
+    private Client client;
+    private Book book;
+    private LocalDate date;
+    private String stated;
+    
+    public Loan(String idLoan,Client client,Book book,LocalDate date,String stated ){
+        this.idLoan=idLoan;
+        this.client=client;
+        this.book=book;
+        this.date=date;
+        this.stated="Activo";
+    }
+    public String getIDLoan(){return idLoan;}
+    public Client getClient(){return client;}
+    public Book getBook(){return book;}
+    public LocalDate getDate(){return date;}
+    public String getStated(){return stated;}
+    
+    public void setIDLoan(String idLoan){this.idLoan=idLoan;}
+    public void setClient(Client client){this.client=client;}
+    public void setBook(Book book){this.book=book;}
+    public void setDate(LocalDate date){this.date=date;}
+    public void setStated(String stated){this.stated=stated;}
+}
